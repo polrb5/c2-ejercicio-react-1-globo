e1092e02fb9cb41462d428ae50b26986b86c17d7
Pol Reig <polreigbroto@gmail.com>
2021-06-10 14:39:46 +0200

    Contador añadido y juego activado

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import { useState } from "react";
 
 function App() {
-  const iconSize = 100;
   const vWidth = window.innerWidth;
   const vHeight = window.innerHeight;
   const top = Math.floor(Math.random() * vHeight);
   const left = Math.floor(Math.random() * vWidth);
 
   const getRandomPosition = () => ({
-    top: top - iconSize,
-    left: left - iconSize,
+    top: top,
+    left: left,
   });
 
   const [posicionActual, setPosicionActual] = useState(getRandomPosition());
+  const [points, setPoints] = useState(0);
 
   const aleatoryPosition = () => {
     setTimeout(() => {
       setPosicionActual(getRandomPosition());
     }, 300);
   };
+  const countPoints = () => {
+    setPoints(points + 1);
+  };
 
   return (
-    <div className="contenedor">
+    <>
+      <p>Puntos: {points}</p>
       <i
         className="icono fab fa-fly"
         style={{ top: posicionActual.top, left: posicionActual.left }}
         onMouseOver={aleatoryPosition}
+        onClick={countPoints}
       ></i>
-    </div>
+    </>
   );
 }
 
