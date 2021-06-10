82826bb51a0d16f376aaa14aa1e90915013d0f40
Pol Reig <polreigbroto@gmail.com>
2021-06-10 14:33:51 +0200

    posición aleatória del globo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-function App() {
-  return (
+import { useState } from "react";
 
+function App() {
+  const iconSize = 100;
+  const vWidth = window.innerWidth;
+  const vHeight = window.innerHeight;
+  const top = Math.floor(Math.random() * vHeight);
+  const left = Math.floor(Math.random() * vWidth);
+
+  const getRandomPosition = () => ({
+    top: top - iconSize,
+    left: left - iconSize,
+  });
+
+  const [posicionActual, setPosicionActual] = useState(getRandomPosition());
+
+  const aleatoryPosition = () => {
+    setTimeout(() => {
+      setPosicionActual(getRandomPosition());
+    }, 300);
+  };
+
+  return (
+    <div className="contenedor">
+      <i
+        className="icono fab fa-fly"
+        style={{ top: posicionActual.top, left: posicionActual.left }}
+        onMouseOver={aleatoryPosition}
+      ></i>
+    </div>
   );
 }
 
